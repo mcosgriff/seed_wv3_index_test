d92792a4084143fc25e11596ae8df915b70dc9ad
Matthew J. Cosgriff <mcosgriff@gmail.com>
2019-09-24 09:42:43 -0600

    Built out remaining classes

--- indexes.py
+++ indexes.py
@@ -33,6 +33,8 @@ class Indexes:
         self.raster_path = raster_path
         self.processed_raster = None
 
+        np.seterr(divide='ignore', invalid='ignore')
+
     def __enter__(self):
         self.raster = rasterio.open(self.raster_path, mode='r', driver='GTiff')
 
@@ -56,7 +58,7 @@ class Indexes:
         return profile
 
     def save_output(self, profile: property, output_ds: np.ndarray) -> str:
-        with rasterio.open(os.path.splitext(self.raster_path)[0] + '_{}.tif'.format(self.index_name()), mode='w',
+        with rasterio.open(os.path.splitext(self.raster_path)[0] + '_{}.tif'.format(self.index_name), mode='w',
                            **profile) as output:
             output.write(output_ds, 1)
 
@@ -104,32 +106,44 @@ class Polymer2(Indexes):
         band_14 = self.get_band_pixel_values(BandTable.SWIR_6)
         band_16 = self.get_band_pixel_values(BandTable.SWIR_8)
 
-        np.seterr(divide='ignore', invalid='ignore')
-        output_ds = np.add(np.divide(band_10, band_11), np.divide(band_14, band_16))
+        self.processed_raster = np.add(np.divide(band_10, band_11), np.divide(band_14, band_16))
 
-        return self.save_output(self.build_output_profile(), linear_percent_stretch(output_ds))
+        return self.save_output(self.build_output_profile(), linear_percent_stretch(self.processed_raster))
 
 
 class Soil(Indexes):
     def __init__(self, raster_path: str):
-        Indexes.__init__(self, raster_path, '')
+        Indexes.__init__(self, raster_path, 'soil_index')
 
     def which_bands_required(self) -> list:
-        return []
+        return [BandTable.GREEN, BandTable.YELLOW]
 
-    def process_index(self) -> np.ndarray:
-        pass
+    def process_index(self) -> str:
+        band_3 = self.get_band_pixel_values(BandTable.GREEN)
+        band_4 = self.get_band_pixel_values(BandTable.YELLOW)
+
+        self.processed_raster = np.divide(np.subtract(band_4, band_3), np.add(band_4, band_3))
+
+        return self.save_output(self.build_output_profile(), linear_percent_stretch(self.processed_raster))
 
 
 class BuiltUp(Indexes):
     def __init__(self, raster_path: str):
-        Indexes.__init__(self, raster_path, '')
+        Indexes.__init__(self, raster_path, 'built_up_index')
 
     def which_bands_required(self) -> list:
-        return []
+        return [BandTable.COASTAL, BandTable.RED_EDGE]
 
-    def process_index(self) -> np.ndarray:
-        pass
+    def process_index(self) -> str:
+        band_1 = self.get_band_pixel_values(BandTable.COASTAL)
+        band_6 = self.get_band_pixel_values(BandTable.RED_EDGE)
+
+        ds_temp_1 = np.subtract(band_1, band_6)
+        ds_temp_2 = np.add(band_1, band_6)
+
+        self.processed_raster = np.where(ds_temp_2 > 0, np.divide(ds_temp_1, ds_temp_2), float('nan'))
+
+        return self.save_output(self.build_output_profile(), self.processed_raster)
 
 
 class NormalizedDifferentialVegetation(Indexes):
@@ -137,29 +151,53 @@ class NormalizedDifferentialVegetation(Indexes):
         Indexes.__init__(self, raster_path, '')
 
     def which_bands_required(self) -> list:
-        return []
+        return [BandTable.RED, BandTable.NEAR_INFRARED_1]
 
-    def process_index(self) -> np.ndarray:
-        pass
+    def process_index(self) -> str:
+        band_5 = self.get_band_pixel_values(BandTable.RED)
+        band_7 = self.get_band_pixel_values(BandTable.NEAR_INFRARED_1)
+
+        temp_ds_1 = np.subtract(band_7, band_5)
+        temp_ds_2 = np.add(band_7, band_5)
+
+        check = np.logical_or(temp_ds_1 > 0, temp_ds_2 > 0)
+        self.processed_raster = np.where(temp_ds_2 > 0, np.divide(temp_ds_1, temp_ds_2), np.nan)
+
+        return self.save_output(self.build_output_profile(), self.processed_raster)
 
 
 class NormalizedDifferentialVegetationRedEdge(Indexes):
     def __init__(self, raster_path: str):
-        Indexes.__init__(self, raster_path, '')
+        Indexes.__init__(self, raster_path, 'ndvi_re')
 
     def which_bands_required(self) -> list:
-        return []
+        return [BandTable.RED, BandTable.RED_EDGE]
 
-    def process_index(self) -> np.ndarray:
-        pass
+    def process_index(self) -> str:
+        band_5 = self.get_band_pixel_values(BandTable.RED)
+        band_6 = self.get_band_pixel_values(BandTable.RED_EDGE)
+
+        temp_ds_1 = np.subtract(band_6, band_5)
+        temp_ds_2 = np.add(band_6, band_5)
+
+        check = np.logical_or(temp_ds_1 > 0, temp_ds_2 > 0)
+        output_ds = np.where(temp_ds_2 > 0, np.divide(temp_ds_1, temp_ds_2), np.nan)
+
+        return self.save_output(self.build_output_profile(), output_ds)
 
 
 class WorldViewWater(Indexes):
     def __init__(self, raster_path: str):
-        Indexes.__init__(self, raster_path, '')
+        Indexes.__init__(self, raster_path, 'wv_water_index')
 
     def which_bands_required(self) -> list:
         return []
 
     def process_index(self) -> np.ndarray:
-        pass
+        band_1 = self.get_band_pixel_values(BandTable.COASTAL)
+        band_8 = self.get_band_pixel_values(BandTable.NEAR_INFRARED_2)
+
+        check = np.logical_or(band_1 > 0, band_8 > 0)
+        self.processed_raster = np.where(check, ((band_1 - band_8) / (band_1 + band_8)), -9)
+
+        return self.save_output(self.build_output_profile(), self.processed_raste)
